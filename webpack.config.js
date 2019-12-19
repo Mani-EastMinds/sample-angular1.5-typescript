318c65685cb9b321a786dceeb7c410ba8e1ca546
Ramineni, Suresh <Suresh.Ramineni@united.com>
2019-12-18 20:53:29 -0600

    initial check in

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './app/app.core.module.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'
+            },
+            { test: /\.css$/, use: 'css-loader' },
+            { test: /\.ts$/, use: 'ts-loader' }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve('index.html') })],
+
+    devServer: {
+        port: 8000,
+        open: 'chrome'
+    }
+}
